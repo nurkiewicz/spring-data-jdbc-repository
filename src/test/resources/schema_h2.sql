e941707ed6fc2b39fdde106c65cddd0c5d95151b
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-12-20 00:13:46 +0100

    Schema for COMMENT table

--- src/test/resources/schema_h2.sql
+++ src/test/resources/schema_h2.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS USER (
 	reputation INT NOT NULL,
 	enabled BOOLEAN NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS COMMENT (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	user_name varchar(256),
+	contents varchar(1000),
+	created_time TIMESTAMP NOT NULL,
+	favourite_count INT NOT NULL
+);
