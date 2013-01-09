427b5d11125b4405c1a18d2f13e435726f608b3e
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-01-09 23:22:10 +0100

    Missing table in MySQL database script

--- src/test/resources/schema_mysql.sql
+++ src/test/resources/schema_mysql.sql
@@ -5,3 +5,12 @@ CREATE TABLE USER (
 	enabled BIT(1) NOT NULL,
 	PRIMARY KEY (user_name)
 );
+
+CREATE TABLE IF NOT EXISTS COMMENT (
+  id INT AUTO_INCREMENT,
+  user_name varchar(256),
+  contents varchar(1000),
+  created_time TIMESTAMP NOT NULL,
+  favourite_count INT NOT NULL,
+  PRIMARY KEY (id)
+);
